82c0a8321b05f7cd2ae47f04101b8a2313c4e744
onesunny2 <ippne996@gmail.com>
2025-01-10 19:51:32 +0900

    [feat] collectionView delegate, datasource 연결, xib 셀들 등록 완료

--- TravelTalkChatting/ViewController/TravelTalkViewController.swift
+++ TravelTalkChatting/ViewController/TravelTalkViewController.swift
@@ -15,8 +15,34 @@ class TravelTalkViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        chatCollectionView.delegate = self
+        chatCollectionView.dataSource = self
+        
+        registerCells()
     }
 
 
+}
+
+
+// MARK: - CollectionView 설정
+extension TravelTalkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func registerCells() {
+        let xib1 = UINib(nibName: SingleChatCollectionViewCell.identifier, bundle: nil)
+        chatCollectionView.register(xib1, forCellWithReuseIdentifier: SingleChatCollectionViewCell.identifier)
+        
+        let xib2 = UINib(nibName: MultiChatCollectionViewCell.identifier, bundle: nil)
+        chatCollectionView.register(xib2, forCellWithReuseIdentifier: MultiChatCollectionViewCell.identifier)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 }
