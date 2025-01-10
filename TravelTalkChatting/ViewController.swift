4aaec23c1a02c334c54fc5c8a08c8dd411546849
onesunny2 <ippne996@gmail.com>
2025-01-10 16:28:50 +0900

    Initial Commit

---
+++ TravelTalkChatting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalkChatting
+//
+//  Created by Lee Wonsun on 1/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
