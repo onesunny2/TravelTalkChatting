c5e3a7a496f1cccd6c9c6226143cb813d4484976
onesunny2 <ippne996@gmail.com>
2025-01-11 18:06:57 +0900

    [design] FriendChat XIB 오토레이아웃으로 구성

---
+++ TravelTalkChatting/TableViewCell/FriendChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendChatTableViewCell.swift
+//  TravelTalkChatting
+//
+//  Created by Lee Wonsun on 1/11/25.
+//
+
+import UIKit
+
+class FriendChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TravelTalkChatting/TableViewCell/MyChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyChatTableViewCell.swift
+//  TravelTalkChatting
+//
+//  Created by Lee Wonsun on 1/11/25.
+//
+
+import UIKit
+
+class MyChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
