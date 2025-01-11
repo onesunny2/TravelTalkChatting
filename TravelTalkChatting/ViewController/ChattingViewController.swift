a7650724de6474c1530085eaf61f2c3303b972c5
onesunny2 <ippne996@gmail.com>
2025-01-11 17:34:41 +0900

    [Design] ChattingViewController 컴포넌트 오토레이아웃 설정 및 로직 파일 연결

---
+++ TravelTalkChatting/ViewController/ChattingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChattingViewController.swift
+//  TravelTalkChatting
+//
+//  Created by Lee Wonsun on 1/11/25.
+//
+
+import UIKit
+
+class ChattingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
